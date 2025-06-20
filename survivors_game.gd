bd1f528d9a2f3f45b0b789ff05de8129b075bc1f
Aldo Utrera <accounts@aldoutrera.com>
2025-06-20 09:33:50 -0500

    Spawn mobs using a timer

--- survivors_game.gd
+++ survivors_game.gd
@@ -1,12 +1,5 @@
 extends Node2D
 
-func _ready() -> void:
-	spawn_mob()
-	spawn_mob()
-	spawn_mob()
-	spawn_mob()
-	spawn_mob()
-	spawn_mob()
 
 func spawn_mob():
 	const MOB = preload("res://mob.tscn")
@@ -14,3 +7,7 @@ func spawn_mob():
 	%PathFollow2D.progress_ratio = randf()
 	new_mob.global_position = %PathFollow2D.global_position
 	add_child(new_mob)
+
+
+func _on_timer_timeout() -> void:
+	spawn_mob()
